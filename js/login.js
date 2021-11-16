4e40e06cd7c7670acc44805421e704c3d82a2a3e
enter <ameenguyen.07@gmail.com>
2021-11-16 23:07:26 +0700

    login popup

--- js/login.js
+++ js/login.js
@@ -1,3 +1,5 @@
+// LOGIN FORM
+
 const loginPopup = document.querySelector(".login-popup");
 const close = document.querySelector(".close");
 
@@ -12,3 +14,24 @@ window.addEventListener("load", function() {
 close.addEventListener("click", function() {
     loginPopup.classList.remove("show");
 })
+
+// LOGIN INPUT
+
+
+
+function validateForm() {
+    // Bước 1: Lấy giá trị của username và password
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
+
+    // Bước 2: Kiểm tra dữ liệu hợp lệ hay không
+    if (email == '') {
+        alert('Enter your email');
+        return false;
+    } else if (password == '') {
+        alert('Enter your passwork');
+        return false;
+    }
+    alert('Success Login');
+    return true;
+}
