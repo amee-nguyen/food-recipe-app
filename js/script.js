68c2698f4dfa9ce8d308e8a7ddd4b73b5cb9af7a
amee-nguyen <ameenguyen.07@gmail.com>
2021-11-21 23:03:46 +0700

    push id to localstorage

--- js/script.js
+++ js/script.js
@@ -54,6 +54,34 @@ function getMealRecipe(e){
     }
 }
 
+// const listenForLikes = () => {
+//     const likes = document.querySelectorAll(".like");
+//     likes.forEach(like => {
+//         like.addEventListener("click", (event) => {
+//             event.target.classList.toggle("like-no");
+//             event.target.classList.toggle("like-yes");
+//             if(event.target.classList.contains("like-yes")) {
+//                 console.log("Saving Favorite");
+//                 getFaveData(event.target);
+//             } else {
+//                 console.log("Removing Favorite");
+//                 getFaveData(event.target);
+//             }
+//         })
+
+//     })
+// }
+
+// const getFaveData = (elem) => {
+//     const parent = elem.parentElement;
+//     const name = parent.querySelector('h2').textContent;
+//     const img = parent.querySelector('img').src;
+//     const faveObj = {img, name};
+//     console.log(faveObj);
+
+// }
+
+
 // create a modal
 function mealRecipeModal(meal){
     console.log(meal);
